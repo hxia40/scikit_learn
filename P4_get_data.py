ec98792b30a92e552c1c5162e77a22075826a39c
Hui Xia <huixia@bmeadmins-MacBook-Pro.local>
2019-08-19 14:30:13 -0500

    df

--- P4_get_data.py
+++ P4_get_data.py
@@ -5,12 +5,13 @@ from datetime import datetime
 
 path = "/Users/huixia/Documents/ScikitLearn/intraQuarter"
 
+
 def Key_Stats(gather="Total Debt/Equity (mrq)"):
     statspath = path+'/_KeyStats'
     stock_list = [x[0] for x in os.walk(statspath)]     #x[0] is directory len=561, x[1] is folder name len = 561, x[2] is html file name, len = 561
     # print len(stock_list)
     # print stock_list
-
+    df = pd.DataFrame(columns = ['Date','Unix','Ticker','DE Ratio'])
     for each_dir in stock_list[1:]:
         print "each_dir:"+ each_dir
         each_file = os.listdir(each_dir)
@@ -28,13 +29,16 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
                 source = open(full_file_path, 'r').read()  #normally this will be a urllib.urlopen if this is a url
                 # print source
                 try:
-                    value = source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
-
+                    value = float(source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+                    df = df.append({'Date':date_stamp,'Unix':unix_time,'Ticker':ticker,'DE Ratio':value,},ignore_index = True)
                 except Exception as e:
-                    print full_file_path
-                    value = float('nan')
-
-                print(ticker+":",value)
+                    pass
+                    # print full_file_path
+                    # value = float('nan')
+    save = gather.replace(' ', '').replace(')', '').replace('(', '').replace('/', '') + str('.csv')
+    print save
+    df.to_csv(save)
+                # print(ticker+":",value)
 
 
             # time.sleep(15)
